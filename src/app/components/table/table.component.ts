e0323ec871db57436791e1a61b6ef0a48faf6c29
Andres <aramirez7310@uta.edu.ec>
2025-04-30 01:12:19 -0500

    Completed tables with supabase

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,7 +1,9 @@
 import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ColDef, Theme, themeQuartz } from 'ag-grid-community';
-
+import { AlertController, ToastController } from '@ionic/angular';
+import { ColDef, Theme, themeQuartz, GridApi } from 'ag-grid-community';
+import { Credit } from 'src/app/models/credit.model';
+import { SupabaseService } from 'src/app/services/supabase.service';
 
 @Component({
   selector: 'app-table',
@@ -12,18 +14,29 @@ import { ColDef, Theme, themeQuartz } from 'ag-grid-community';
 export class TableComponent  implements OnInit {
   @ViewChild('agGrid', { read: ElementRef }) agGridRef!: ElementRef;
   fb = inject(FormBuilder);
+  supabase = inject(SupabaseService);
+  toastCtrl = inject(ToastController);
+  alertCtrl = inject(AlertController);
+
   creditForm: FormGroup;
+  showTasa = false;
   showAmount = false;
   showTerm = false;
   showTermOption = false;
   typeTable = '';
   showTable = false;
+  paginationEnabled = true;
 
-  creditTypes = [
-    { value: 'personal', label: 'Crédito Personal' },
-    { value: 'hipotecario', label: 'Crédito Hipotecario' },
-    { value: 'automotriz', label: 'Crédito Automotriz' },
-  ];
+  credits: [] = [];
+  tasa = 0;
+  minCredit = 0;
+  maxCredit = 0;
+  minTime = 0;
+  maxTime = 0;
+
+  creditChannel: any;
+
+  creditTypes: Credit[] = [];
   
   myTheme = themeQuartz.withParams({
     backgroundColor: '#f5f7fa',                   // Gris claro para fondo general
@@ -59,9 +72,28 @@ export class TableComponent  implements OnInit {
   ];
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.getCredits();
+
+    this.creditChannel = this.supabase.subscribeToCreditChanges((payload) => {
+      if (payload.eventType === 'INSERT' || payload.eventType === 'UPDATE' || payload.eventType === 'DELETE') {
+        const newCredit = payload.new;
+        if (payload.eventType === 'INSERT') {
+          this.creditTypes = [...this.creditTypes, newCredit];
+        } else if (payload.eventType === 'UPDATE') {
+          this.creditTypes = this.creditTypes.map(credit =>
+            credit.id === newCredit.id ? newCredit : credit
+          );
+        } else if (payload.eventType === 'DELETE') {
+          const deletedCredit = payload.old;
+          this.creditTypes = this.creditTypes.filter(credit => credit.id !== deletedCredit.id);
+        }
+      }
+    });
+
     this.creditForm = this.fb.group({
       creditType: [null, Validators.required],
+      tasa: [{ value: '', disabled: true }, Validators.required],
       amount: [{ value: '', disabled: true }, [
         Validators.required,
         Validators.pattern(/^\d+(\.\d{1,2})?$/)
@@ -76,6 +108,20 @@ export class TableComponent  implements OnInit {
     // Al seleccionar tipo, habilitar monto
     this.creditForm.get('creditType').valueChanges.subscribe(val => {
       if (val) {
+        let credit: Credit = this.creditTypes.find(credit => credit.id === val)
+        this.tasa= credit.anualRate;
+        this.creditForm.get('tasa').setValue(this.tasa+'%');
+        this.minCredit = credit.minCredit;
+        this.maxCredit = credit.maxCredit;
+        this.minTime = credit.minTime;
+        this.maxTime = credit.maxTime;
+        this.showTasa = true;
+        this.creditForm.get('amount').setValidators([
+          Validators.required,
+          Validators.pattern(/^\d+(\.\d{1,2})?$/),
+          Validators.min(this.minCredit),
+          Validators.max(this.maxCredit)
+        ]);
         this.creditForm.get('amount').enable();
         this.showAmount = true;
         this.creditForm.get('termUnit').disable();
@@ -105,8 +151,12 @@ export class TableComponent  implements OnInit {
         this.creditForm.get('term').enable();
         this.showTerm = true;
         // Ajustar validadores de rango según unidad
-        const min = 1;
-        const max = unit === 'years' ? 30 : 360;
+        let min = this.minTime;
+        let max = this.maxTime;
+        if (unit === 'years') {
+          min = this.minTime /12;
+          max = this.maxTime /12;
+        }
         this.creditForm.get('term').setValidators([
           Validators.required,
           Validators.min(min),
@@ -183,6 +233,7 @@ export class TableComponent  implements OnInit {
   }
 
   imprimirTabla() {
+    this.paginationEnabled = false;
     const gridElement = this.agGridRef?.nativeElement;
   
     if (!gridElement) {
@@ -199,9 +250,9 @@ export class TableComponent  implements OnInit {
   
     // Datos del formulario
     const { amount, term, termUnit, creditType } = this.creditForm.value;
-    const tasaAnual = 0.12; // O toma tu tasa real si es dinámica
-    const tasaMensual = (tasaAnual / 12) * 100;
-  
+    const tasaAnual = this.tasa; // O toma tu tasa real si es dinámica
+    const tasaMensual = (tasaAnual / 12);
+    const credit: Credit = this.creditTypes.find(credit => credit.id === creditType)
     const ventana = window.open('', '', 'width=1200,height=800');
     if (ventana) {
       ventana.document.write(`
@@ -237,10 +288,10 @@ export class TableComponent  implements OnInit {
           <body>
             <h2>Tabla de Amortización - ${this.typeTable}</h2>
             <div class="datos-creditos">
-              <p><strong>Tipo de Crédito:</strong> ${creditType}</p>
+              <p><strong>Tipo de Crédito:</strong> ${creditType} - ${credit.name}</p>
               <p><strong>Monto:</strong> $${Number(amount).toFixed(2)}</p>
               <p><strong>Plazo:</strong> ${term} ${termUnit === 'years' ? 'años' : 'meses'}</p>
-              <p><strong>Interés Mensual:</strong> ${tasaMensual.toFixed(2)}%</p>
+              <p><strong>Tasa de Interés:</strong> ${termUnit ==='years' ? tasaAnual + '% anual' : tasaMensual.toFixed(2) +'% mensual'}</p>
             </div>
             <div class="ag-theme-alpine">
               ${tablaHTML}
@@ -256,6 +307,42 @@ export class TableComponent  implements OnInit {
         ventana.close();
       }, 1000);
     }
+    this.paginationEnabled = true;
+  }
+
+  private async getCredits() {
+    try {
+      const { data, error } = await this.supabase.getCredits();
+      if (error) throw error;
+      if (data) {
+        this.creditTypes = data;
+      }
+    } catch (err) {
+      const toast = await this.toastCtrl.create({
+        message: 'Error al cargar creditos: '+ err.message,
+        duration: 2000,
+        color: 'danger'
+      });
+      await toast.present();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.creditChannel) {
+      this.creditChannel.unsubscribe();
+    }
   }
   
+
+  async showCreditInfo(credit: Credit) {
+    const alert = await this.alertCtrl.create({
+      header: `Crédito ${credit.name}`,
+      message: `
+        Información: ${credit.information}
+      `,
+      buttons: ['Cerrar'] // Botón para cerrar la alerta
+    });
+
+    await alert.present();
+  }
 }
